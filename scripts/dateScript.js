ba7d865403e8520b991475d59316d81858e4b564
haydenallred <haydeythepooh@gmail.com>
2019-09-21 01:50:25 -0600

    added current year functionality

--- scripts/dateScript.js
+++ scripts/dateScript.js
@@ -1,2 +1,5 @@
 var modifyDate = document.lastModified;
-document.getElementById("dateModified").textContent = modifyDate;
+document.getElementById("dateModified").innerHTML = modifyDate;
+
+var currentYear = new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = currentYear;
