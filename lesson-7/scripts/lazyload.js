8d6d84675a8d44573c265709d89f50d01c27c179
haydenallred <haydeythepooh@gmail.com>
2019-10-31 22:16:52 -0600

    Lesson 07 - Lazy Load

---
+++ lesson-7/scripts/lazyload.js
@@ -0,0 +1,34 @@
+
+
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+}
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+      image.removeAttribute('data-src');
+    };
+  };
+
+if('IntersectionObserver' in window) {
+	const observer = new IntersectionObserver(function(items, observer) {
+		items.forEach(function(item) {
+			if(item.isIntersecting) {
+				loadImages(item.target);
+				observer.unobserve(item.target);
+			}
+		}, imgOptions);
+	});
+	imagesToLoad.forEach(function(img) {
+		observer.observe(img);
+	});
+}
+else {
+	imagesToLoad.forEach(function(img) {
+		loadImages(img);
+	});
+}
