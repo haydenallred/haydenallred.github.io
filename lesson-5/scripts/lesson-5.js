9c029ce4dca6632b6911928538b4bda7e3586b27
haydenallred <haydeythepooh@gmail.com>
2019-11-02 15:36:24 -0600

    Lesson 05

---
+++ lesson-5/scripts/lesson-5.js
@@ -0,0 +1,22 @@
+let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let monthsOfTheYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+let date = new Date(document.lastModified);
+let day = date.getDate();
+let dayName = daysOfTheWeek[date.getDay()];
+let month = monthsOfTheYear[date.getMonth()];
+let year = date.getFullYear();
+
+let lastUpdated = dayName + ", " + day + " " + month + " " + year;
+
+let lastUpdatedElement = document.getElementById("last-updated");
+lastUpdatedElement.textContent = "Last Updated " + lastUpdated;
+
+function toggleMenu() {
+    let menu = document.getElementById("menu");
+    menu.classList.toggle("expanded");
+}
+
+if (dayName != "Friday") {
+    document.getElementById("alert").style.display = "none";
+}
