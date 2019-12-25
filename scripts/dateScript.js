9f695317b6d4d0532b1f7f346d80d05742e7ba5d
haydenallred <haydeythepooh@gmail.com>
2019-12-24 17:22:57 -0700

    Delete CIT 230 Information

--- scripts/dateScript.js
+++
@@ -1,5 +0,0 @@
-var modifyDate = document.lastModified;
-document.getElementById("dateModified").innerHTML = modifyDate;
-
-var currentYear = new Date().getFullYear();
-document.getElementById("currentYear").innerHTML = currentYear;
