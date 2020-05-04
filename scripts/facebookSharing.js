5755a77e527f854045e1763c62ac52fd5a4e3c60
haydenallred <haydeythepooh@gmail.com>
2020-05-04 17:08:26 -0600

    Update facebookSharing.js

--- scripts/facebookSharing.js
+++ scripts/facebookSharing.js
@@ -2,7 +2,7 @@ function postToFeed() {
     // calling the API ...
     var obj = {
       method: 'feed',
-      link: 'https://www.azeezkallayi.com/',
+      link: 'https://www.haydenallred.github.io/results.html',
       description: "description goes here",
       picture: 'https://www.azeezkallayi.com/demo/test/womens-day.jpg',
       name: 'International womens day'       
