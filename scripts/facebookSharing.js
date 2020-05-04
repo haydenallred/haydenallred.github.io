d2151be936c2db1a7048176274ea95b798544cff
haydenallred <haydeythepooh@gmail.com>
2020-05-04 17:01:28 -0600

    fb ui

--- scripts/facebookSharing.js
+++ scripts/facebookSharing.js
@@ -1,1 +1,11 @@
-document.querySelector('meta[property="og:description"]').setAttribute("content", "Hey, Javascript is working!");
+function postToFeed() {
+    // calling the API ...
+    var obj = {
+      method: 'feed',
+      link: 'https://www.azeezkallayi.com/',
+      description: "description goes here",
+      picture: 'https://www.azeezkallayi.com/demo/test/womens-day.jpg',
+      name: 'International womens day'       
+    };
+     FB.ui(obj);
+   }
